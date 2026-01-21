81de22b020aa5b5c4f28017846fdf4491473a941
Paul Bogard <paulbogard@gmail.com>
2026-01-20 19:27:20 -0500

    Retrieve Flight Historian recent flights

--- flight_log.py
+++ flight_log.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import requests
 from dotenv import load_dotenv
 
 # Load environment variables from .env file.
@@ -19,7 +20,17 @@ def import_recent():
         raise KeyError(
             "Environment variable API_KEY_FLIGHT_HISTORIAN is missing."
         )
-    print("Flight Historian API key is set.")
+    headers = {"api-key": api_key_fh}
+    url = "https://www.flighthistorian.com/api/recent_flights"
+    print(f"🌐 Requesting {url}")
+    response = requests.get(url, headers=headers, timeout=10)
+    response.raise_for_status()
+    json_data = response.json()
+    if len(json_data) == 0:
+        print("Flight Historian provided zero recent flights.")
+        quit()
+    for flight in json_data:
+        print(flight)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
